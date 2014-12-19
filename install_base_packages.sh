20334e7b875e1991132a35f71cffa0e4c6e2b602
Joao Figueiredo <joaonvfigueiredo@gmail.com>
2014-12-19 14:52:04 +0000

    added extra install dependencies

--- install_base_packages.sh
+++ install_base_packages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y flashplugin-installer tmux vim git
+apt-get install -y flashplugin-installer tmux vim git postgresql-server-dev-all python-dev python-pip virtualenvwrapper postgresql-client postgresql cmake guake thunderbird tshark wireshark
