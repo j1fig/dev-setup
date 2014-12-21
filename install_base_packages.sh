717af94fbb14ee26f01c8501af623b2cfb57fb30
Joao Figueiredo <joaonvfigueiredo@gmail.com>
2014-12-21 20:36:43 +0000

    added missing flash plugin installer

--- install_base_packages.sh
+++ install_base_packages.sh
@@ -3,7 +3,7 @@
 apt-get update
 
 # utils
-apt-get install -y tmux vim git guake thunderbird pavucontrol chromium-browser
+apt-get install -y tmux vim git guake thunderbird pavucontrol chromium-browser flashplugin-installer
 
 # databases
 apt-get install -y postgresql-server-dev-all postgresql-client postgresql redis-server redis-tools
