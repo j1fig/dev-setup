5ba3bcb92667ca1ec36e2c9d89918ed62d596e06
Joao Figueiredo <joaonvfigueiredo@gmail.com>
2014-12-21 23:19:12 +0000

    added vlc to deps

--- install_base_packages.sh
+++ install_base_packages.sh
@@ -3,7 +3,7 @@
 apt-get update
 
 # utils
-apt-get install -y tmux vim git guake thunderbird pavucontrol chromium-browser flashplugin-installer
+apt-get install -y tmux vim git guake thunderbird pavucontrol chromium-browser flashplugin-installer vlc
 
 # databases
 apt-get install -y postgresql-server-dev-all postgresql-client postgresql redis-server redis-tools
