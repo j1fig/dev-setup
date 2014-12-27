1dff5efdab3e370268fc1d607b6e86e596fe42a8
Joao Figueiredo <joaonvfigueiredo@gmail.com>
2014-12-27 16:39:03 +0000

    added gimp to deps

--- install_base_packages.sh
+++ install_base_packages.sh
@@ -3,7 +3,7 @@
 apt-get update
 
 # utils
-apt-get install -y tmux vim git guake thunderbird pavucontrol chromium-browser flashplugin-installer vlc skype
+apt-get install -y tmux vim git guake thunderbird pavucontrol chromium-browser flashplugin-installer vlc skype gimp
 
 # databases
 apt-get install -y postgresql-server-dev-all postgresql-client postgresql redis-server redis-tools
